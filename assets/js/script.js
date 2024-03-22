009146b5ad35b296b1879ca9175210d00ecb8a94
Asif Zulfiqar <asifzulfiqar43@gmail.com>
2024-03-22 11:34:31 +0500

    doughnut chart added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-const ctx = document.getElementById('pieChart');
+const pieChart = document.getElementById('pieChart');
 
 const pieChartData = {
     labels: [
@@ -18,23 +18,96 @@ const pieChartData = {
     }]
   };
 
-  const myPieChart = new Chart(ctx, {
+  const myPieChart = new Chart(pieChart, {
     type: 'pie',
     data: pieChartData,
+    options: {
+        plugins: {
+            legend: {
+                display: false,
+            }
+        }
+    }
   })
 
-  const pieChartContainer = document.querySelector('#pieChartValues');
-  var totalpieCharValues = pieChartData.datasets[0].data.reduce((acc, curr) => acc + curr, 0);
+const pieChartContainer = document.querySelector('#pieChartValues');
 
-  const pieChartValues = pieChartData.labels.map((label, index) => {
-    const pieCharData = pieChartData.datasets[0].data[index];
+const totalPieChartData = pieChartData.datasets[0].data.reduce((acc, curr) => acc + curr, 0);
+
+document.querySelector('#pieCharTotal').innerHTML = `<h6 class="f-sm">Total Room: ${totalPieChartData}</h6>`;
+
+const dataLength = pieChartData.datasets[0].data.length;
+
+const pieChartValues = pieChartData.datasets[0].data.map((value, index) => {
+    const label = pieChartData.labels[index];
+    const color = pieChartData.datasets[0].backgroundColor[index];
 
     return `
         <div>
-            <h4>Total Room: ${totalpieCharValues}</h4>
-            <p>${label}- ${index}<p>
+            <div class="d-flex align-items-center flex-gap-1">
+                <div style="width:10px; height:10px; background-color:${color}; border-radius:50%;"></div>
+                <p class="f-sm">${label}- ${value}</p>
+            </div>
         </div>
-        `
-  }).join('');
+    `;
+}).join('');
 
-  pieChartContainer.innerHTML = pieChartValues;
+pieChartContainer.innerHTML = pieChartValues;
+
+
+
+// Doughnut Chart 
+const Doughnut = document.querySelector('#Doughnut');
+
+const doughnutData = {
+    labels: [
+        'Test one',
+        'Test two',
+        'Test three',
+        'Test four'
+    ],
+    datasets: [{
+        data: [4, 2, 1, 1],
+        backgroundColor: [
+            '#02dcff',
+            '#7553ff',
+            '#ffcc43',
+            '#ff6f93'
+
+        ],
+        hoverOffset: 1
+    }]
+};
+
+
+const doughnutTotalPlugin = {
+    afterDraw: function(chart) {
+        const ctx = chart.ctx;
+        const canvas = chart.canvas;
+        const chartArea = chart.chartArea;
+        const sum = chart.data.datasets[0].data.reduce((acc, value) => acc + value, 0);
+
+        // Display total in the center
+        ctx.save();
+        ctx.fillStyle = '#000000'; // Set the color of the text
+        ctx.font = '10px Arial'; // Set the font size and family
+        ctx.textAlign = 'center';
+        ctx.textBaseline = 'middle';
+        ctx.fillText(sum, (chartArea.left + chartArea.right) / 2, (chartArea.top + chartArea.bottom) / 2);
+        ctx.restore();
+    }
+};
+
+const myDoughnut = new Chart(Doughnut, {
+    type: 'doughnut',
+    data: doughnutData,
+    options: {
+        cutout: '80%',
+        plugins: {
+            doughnutTotal : doughnutTotalPlugin,
+            legend: {
+                display: false,
+            }
+        }
+    }
+})
